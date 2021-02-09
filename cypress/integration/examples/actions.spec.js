c710dd4b3a3eb1733b84f1ac7277cae39f7012e3
Gururaj <gururaj@Gururajs-MacBook-Pro.local>
2021-02-09 19:17:05 +0000

    gcp push

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -8,7 +8,7 @@ context('Actions', () => {
   // https://on.cypress.io/interacting-with-elements
 
   it('.type() - type into a DOM element', () => {
-    // https://on.cypress.io/type
+ 
     cy.get('.action-email')
       .type('fake@email.com').should('have.value', 'fake@email.com')
 
